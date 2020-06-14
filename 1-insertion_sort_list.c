8168cfc86a888bc4a1b81729798280282971a4cc
yacinekedidi <kedidiyacine@gmail.com>
2020-06-14 16:58:47 +0100

    C

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,16 @@
 #include "sort.h"
 
 /**
- * swap - Entry point
- * @back: node
- * @current: node
- * Return: Always
- */
-void swap(listint_t *back, listint_t *current)
-{
-	if (back->prev)
-		back->prev->next = current;
-	if (current->next)
-		current->next->prev = current->prev;
-	back->next = current->next;
-	current->prev = back->prev;
-	back->prev = current;
-	current->next = back;
-}
-
-
-/**
  * insertion_sort_list - Entry point
  * @list: head
- * Return: Always 0
+ * Return: Always
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *loop, *current;
+	listint_t *loop, *back, *tmp, *current;
 
 
-	if (!*list || !list || !(*list)->next)
+	if (!list || !*list || !(*list)->next)
 		return;
 	loop = (*list)->next;
 
@@ -37,18 +18,25 @@ void insertion_sort_list(listint_t **list)
 	{
 		current = loop;
 		loop = loop->next;
-		while (current && current->prev)
+		back = current->prev;
+		while (back)
 		{
-
-			if (current->prev->n > current->n)
+			tmp = back->prev;
+			if (back->n > current->n)
 			{
-				swap(current->prev, current);
+				if (back->prev)
+					back->prev->next = current;
+				if (current->next)
+					current->next->prev = back;
+				back->next = current->next;
+				current->prev = back->prev;
+				back->prev = current;
+				current->next = back;
 				if (!current->prev)
 					*list = current;
 				print_list(*list);
 			}
-			else
-				current = current->prev;
+			back = tmp;
 		}
 	}
 }
