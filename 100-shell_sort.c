530a91b4863eef2fade1cfbd9f3d751c9ee5a641
yacinekedidi <kedidiyacine@gmail.com>
2020-06-15 06:12:53 +0100

    C

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+
+/**
+ * shell_sort - Entry point
+ * @a: arr
+ * @size: size
+ * Return: Always 0
+ */
+
+void shell_sort(int *a, size_t size)
+{
+	size_t gap = 1, i, j, tmp;
+
+	if (!a || !size)
+		return;
+	while (gap < (size / 3))
+		gap = gap * 3 + 1;
+
+	while (gap >= 1)
+	{
+		for (i = gap ; i < size ; i++)
+		{
+			for (j = i ; j >= gap && a[j - gap] > a[j] ; j -= gap)
+			{
+				tmp = a[j];
+				a[j] = a[j - gap];
+				a[j - gap] = tmp;
+			}
+		}
+		gap /= 3;
+		print_array(a, size);
+	}
+
+}
