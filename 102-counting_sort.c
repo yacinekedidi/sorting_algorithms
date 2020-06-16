0529e23afe502e932e3f0ce567e2b2480e292cc1
yacinekedidi <kedidiyacine@gmail.com>
2020-06-16 01:00:05 +0100

    C

---
+++ 102-counting_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+
+/**
+ * counting_sort - Entry point
+ * @array: arr
+ * @size: s
+ * Return: Always 0
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *a, max = array[0], j, *b;
+	size_t i;
+
+	if (!array || size < 2)
+		return;
+	i = 0;
+	while (i < size)
+	{
+		if (array[i] > max)
+			max = array[i];
+		i++;
+	} a = malloc(sizeof(int) * (max + 1));
+	if (!a)
+		return;
+	for (j = 0 ; j < (max + 1) ; j++)
+		a[j] = 0;
+	for (i = 0 ; i < size ; i++)
+		a[array[i]] += 1;
+
+	for (j = 1 ; j < max + 1 ; j++)
+		a[j + 1] = a[j + 1] + a[j];
+	print_array(a, max + 1);
+	b = malloc(sizeof(int) * size);
+	if (!b)
+	{
+		free(a);
+		return;
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		b[a[array[i]]-- - 1] = array[i];
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		array[i] = b[i];
+	}
+	free(a);
+	free(b);
+}
